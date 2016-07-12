fbaa344cc0b746bbbc062009fe463ce643cd9ec9
Stu Blair <stu314159@gmail.com>
2016-07-12 15:42:59 +0000

    golfball implementation

--- stu_geom.py
+++ stu_geom.py
@@ -11,19 +11,19 @@ d_golf_ball = 0.0427 # meters
 aLx_p = 0.16 
 aLy_p = 0.16
 aLz_p = 0.3
-aN_divs = 80
+aN_divs = 120
 
 # construct the basic sphere
 sphereB = fc.SphereObstruction(d_golf_ball/2., aLx_p/2., aLy_p/2., aLz_p/2.)
 
-d_dimp = d_golf_ball/10.
+d_dimp = d_golf_ball/8.
 
 # set the dimply 75% of its radius from surface
-rd_dimp = (d_golf_ball/2.)+(d_dimp/2.)*0.75 
+rd_dimp = (d_golf_ball/2.)+(d_dimp/2.)*0.65 
 circ_golf_ball = np.pi*d_golf_ball;
 N_dimp_max = np.floor(circ_golf_ball/d_dimp)
 
-N_e = int(N_dimp_max*0.7)
+N_e = int(N_dimp_max*0.6)
 N_a = 2*N_e-1 # for now, assume they are the same density in each direction
 # since the azimuths will go all the way around and elevations
 # only half way; use this formulation
@@ -34,5 +34,6 @@ golfB = fc.GolfBall(sphereB,d_dimp,rd_dimp,N_e,N_a)
 golfChannel = fc.FluidChannel(Lx_p = aLx_p,Ly_p = aLy_p,Lz_p = aLz_p,
                                N_divs = aN_divs, obst = golfB)
 
-golfChannel.write_bc_vtk()
+#golfChannel.write_bc_vtk()
+golfChannel.write_mat_file()
 
