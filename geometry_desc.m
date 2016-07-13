77e5a381fc5f28c2c24371d83d44d8ebeb300ba0
Earl Pascua <m174806@usna.edu>
2016-07-13 14:08:10 +0000

    initial commit on this branch: basic implementation of NACA airfoil modelling capability

--- geometry_desc.m
+++ geometry_desc.m
@@ -4,15 +4,15 @@ close 'all'
 
 %% overall channel dimensions
 Lx_p = 1;
-Ly_p = 1;
-Lz_p = 8;
+Ly_p = 2;
+Lz_p = 10;
 
-Ny_divs = 31;
+Ny_divs = input('Ny_divs? Must be ODD-numbered \n\n');clc;
 
 plot_geom = true;
 
 %% select fluid for the simulation
-fluid = 1;
+fluid = menu('Fluid?','glycerin','glycol','water');
 % 1 = glycerin
 % 2 = glycol
 % 3 = water
@@ -32,13 +32,15 @@ switch fluid
         
 end
 
-obstacle = 5;
+obstacle = menu('Which Obstacle?','None','Cylinder from Top to Bottom','Cylinder:Partial Height'...
+    ,'Sphere','Cone','Cone With Angle of Attack','NACA Airfoil') - 1;
 % 0 = no obstacle
 % 1 = cylinder, bottom to top
 % 2 = cylinder, partial height
-% 3 = cylindrical piling with elliptical scour pit
-% 4 = sphere
-% 5 = cylindrical piling, no scour, double height
+% 3 = sphere
+% 4 = cone
+% 5 = cone with angle of attack
+% 6 = NACA Airfoil
 
 switch obstacle
     case 0
@@ -56,45 +58,43 @@ switch obstacle
        y_max = 0.75*Ly_p;
        cyl_rad = 0.1*Lx_p;
        Lo = cyl_rad*2;
-
+       
     case 3
-       x_c = 0.5*Lx_p; %Center of channel (width)
-       z_c = 0.5*Lz_p; %Center of channel (length)
-       cyl_rad = 0.1*Lx_p; %Piling radius
-       Lo = cyl_rad*2;
-
-	%Scour Pit Dimensions
-	ellip_a = 2*2*cyl_rad; %2x diameter of pile
-	ellip_b = 1*2*cyl_rad; %1x diam of pile
-	ellip_c = 4*2*cyl_rad; %4x diam of pile
-	ellip_x = x_c; %center of pile (width)
-	ellip_z = z_c + cyl_rad; %center located at back of pile (length)
-	ellip_y = ellip_b; %bottom of ellipse at ym
-          
-    case 4
        x_c = 0.5*Lx_p;
        y_c = 0.5*Ly_p;
        z_c = 0.75*Lz_p;
-       sphere_rad = 0.1*Ly_p;
+       sphere_rad = 0.1*Ly_p;    
        Lo = sphere_rad*2;
        
- 
-    case 5  % piling, smooth bottom, double height
-    
-       Ly_p = 2*Ly_p;
-       x_c = 0.5*Lx_p; %Center of channel (width)
-       z_c = 0.5*Lz_p; %Center of channel (length)
-       cyl_rad = 0.1*Lx_p; %Piling radius
-       Lo = cyl_rad*2;
-
-% 	%Scour Pit Dimensions
-% 	ellip_a = 2*2*cyl_rad; %2x diameter of pile
- 	ellip_b = 1*2*cyl_rad; %1x diam of pile
-% 	ellip_c = 4*2*cyl_rad; %4x diam of pile
-% 	ellip_x = x_c; %center of pile (width)
-% 	ellip_z = z_c + cyl_rad; %center located at back of pile (length)
-% 	ellip_y = ellip_b; %bottom of ellipse at ym
-      
+    case 4
+        x_c = 0.5*Lx_p;
+        y_c = 0.5*Ly_p;
+        r_max = 0.25*Lx_p; r_min = 0; 
+        z_min = 0.4*Lz_p;
+        z_max = 0.6*Lz_p;
+        cone_len = z_max - z_min;
+        Lo = cone_len;
+        
+    case 5
+        angle_of_attack = 0;
+        x_c = 0.5*Lx_p;
+        r_max = 0.25*Lx_p; r_min = 0;
+        z_c_max = 0.6*Lz_p-(0.6 - 0.5)*Lz_p*cosd(angle_of_attack);
+        z_c_min = 0.5*Lz_p - (0.5*Lz_p - 0.4*Lz_p)*cosd(angle_of_attack);
+        cone_len = z_c_max - z_c_min;
+        Lo = cone_len;
+        y_c_zmin = 0.5*Ly_p + (0.5-0.4)*Lz_p*sind(angle_of_attack);
+        y_c_zmax = 0.5*Ly_p - (0.5-0.4)*Lz_p*sind(angle_of_attack);
+        
+    case 6 
+        NACA_foil_designator = input('Type in the FOUR-digit NACA Airfoild Designator \n\n','s');
+        z_min = 2;
+        z_max = 5;
+        x_c = 0.5*Lx_p;
+        chordlength = z_max - z_min; % shouldn't be longer than 3 because it has to fit in channel of length 8
+        Lo = chordlength;
+        
+             
 end
 
 %% generate the lattice discretization
@@ -102,9 +102,9 @@ xm = 0; xp = Lx_p;
 ym = 0; yp = Ly_p;
 zm = 0; zp = Lz_p;
 
-Ny = ceil((Ny_divs-1)*(Ly_p/Lo))+1;
-Nx = ceil((Ny_divs-1)*(Lx_p/Lo))+1;
-Nz = ceil((Ny_divs-1)*(Lz_p/Lo))+1;
+Ny = ceil((Ny_divs-1)*(Ly_p./Lo))+1;
+Nx = ceil((Ny_divs-1)*(Lx_p./Lo))+1;
+Nz = ceil((Ny_divs-1)*(Lz_p./Lo))+1;
 
 [gcoord,~,faces]=Brick3Dr2(xm,xp,ym,yp,zm,zp,Nx,Ny,Nz);
 [nnodes,~]=size(gcoord);
@@ -113,7 +113,7 @@ Nz = ceil((Ny_divs-1)*(Lz_p/Lo))+1;
 snl = [faces.zx_m; faces.zx_p; faces.zy_m; faces.zy_p]; snl = snl(:);
 snl = unique(snl);
 
-%% get inlet nodes
+%% get in
 inl = faces.xy_m; 
 inl = setxor(inl,intersect(inl,snl)); % eliminate solid nodes from inl
 
@@ -133,58 +133,58 @@ switch obstacle
       
        obst_list = find((((gcoord(:,1) - x_c).^2 + (gcoord(:,3)-z_c).^2) < cyl_rad*cyl_rad) & ...
          (gcoord(:,2) < y_max));
-
-    case 3
-       %% get solid nodes...bottom is thick, top is single plate
-       snl = find(gcoord(:,2)<ellip_b); snl =snl(:);
-       snl = unique(snl);
-
-       %% get inlet nodes
-       inl = faces.xy_m; 
-       inl = setxor(inl,intersect(inl,snl)); % eliminate solid nodes from inl
-
-       %% get outlet nodes
-	onl = faces.xy_p;
-	onl = setxor(onl,intersect(onl,snl)); %eliminate solid nodes from onl
-
-       scourpit = find(((gcoord(:,1) - ellip_x).^2/ellip_a.^2) + ((gcoord(:,2)-ellip_y).^2/ellip_b.^2) + ((gcoord(:,3) - ellip_z).^2/ellip_c.^2) <= 1);
-
-      %Remove ellipsoid nodes from bottom of channel.
-      snl = setxor(snl,intersect(snl,scourpit));
-
-      % add the cylindrical obstacle to the obstacle list
-      obst_list = find(((gcoord(:,1) - x_c).^2 + (gcoord(:,3)-z_c).^2) < cyl_rad*cyl_rad);
      
-    case 4
+     
+    case 3
         
         obst_list = find(((gcoord(:,1) - x_c).^2 + (gcoord(:,2) - y_c).^2 + ...
             (gcoord(:,3) - z_c).^2 < sphere_rad*sphere_rad));
         
+    case 4 
+        
+        % z >= z_min; z<= z_max and distance from x_c, y_c within r(z)
+        r_z = @(z) (z - z_min).*(r_max/cone_len);
+        obst_list = find((gcoord(:,3)>=z_min) & (gcoord(:,3)<=z_max) & ... % z>=z_min and z<=z_max
+            ((gcoord(:,1)-x_c).^2 + (gcoord(:,2)-y_c).^2 <= r_z(gcoord(:,3)).^2)); 
+        
     case 5
-       %% get solid nodes...bottom is thick, top is single plate
-       snl = find(gcoord(:,2)<ellip_b); snl =snl(:);
-       snl = unique(snl);
+        r_z = @(z) (z - z_c_min).*(r_max/cone_len);%?????
+        y_c = @(z) (z - z_c_min).*(y_c_zmax - y_c_zmin)/cone_len + y_c_zmin; %?????
+        z_max = @(y) (y).*(tand(angle_of_attack)) + z_c_max - 0.5.*Ly_p.*tand(angle_of_attack);
+        
+        obst_list = find((gcoord(:,3)>=z_c_min) & (gcoord(:,3)<=z_max(gcoord(:,2))) & ...
+            ((gcoord(:,1) - x_c).^2 + (gcoord(:,2) -y_c(gcoord(:,3))).^2<= r_z(gcoord(:,3)).^2));
 
-       %% get inlet nodes
-       inl = faces.xy_m; 
-       inl = setxor(inl,intersect(inl,snl)); % eliminate solid nodes from inl
+    case 6
+        z_indices = find(gcoord(:,3) >= z_min & gcoord(:,3) <= z_max);
+        z_values = gcoord(z_indices,3);
+        foilDesignator = num2str(NACA_foil_designator);
+        [upperSurface,lowerSurface] = NACA_plot(foilDesignator,z_values,chordlength);
+        
+        %values of the upper surface
+        z_upper = upperSurface(:,1) + z_min;
+        y_upper = upperSurface(:,2) + 0.5*Ly_p;
+        upperSurface = [z_upper,y_upper]; 
+        
+        %values of lower surface
+        z_lower = lowerSurface(:,1) + z_min;
+        y_lower = lowerSurface(:,2) + 0.5*Ly_p;
+        lowerSurface = [z_lower, y_lower];
 
-       %% get outlet nodes
-	onl = faces.xy_p;
-	onl = setxor(onl,intersect(onl,snl)); %eliminate solid nodes from onl
+        %replace y_values of gcoord with y_lower and y_upper at z_indices
+        gcoordupper = gcoord(:,:);
+        gcoordlower = gcoord(:,:);
+        gcoordupper(z_indices,2) = y_upper; 
+        gcoordlower(z_indices,2) = y_lower;
 
-%        scourpit = find(((gcoord(:,1) - ellip_x).^2/ellip_a.^2) + ((gcoord(:,2)-ellip_y).^2/ellip_b.^2) + ((gcoord(:,3) - ellip_z).^2/ellip_c.^2) <= 1);
-% 
-%       %Remove ellipsoid nodes from bottom of channel.
-%       snl = setxor(snl,intersect(snl,scourpit));
-
-      % add the cylindrical obstacle to the obstacle list
-      obst_list = find(((gcoord(:,1) - x_c).^2 + (gcoord(:,3)-z_c).^2) < cyl_rad*cyl_rad);
+        x_min = 0;
+        x_max = Lx_p;
+        obst_list = find((gcoord(:,2) <= gcoordupper(:,2) & gcoord(:,2) >= gcoordlower(:,2)) &...
+            (gcoord(:,3) >= z_min & gcoord(:,3) <= z_max));
+        
 end
-
-% add obstacle nodes to the solid node list
+% add obstacle nodes to the solid node listy_c = @(z) (z - z_c_min).*(y_c_min/cone_len);
 snl = union(snl,obst_list);
-
 
 %% find pressure reference lattice point
 dx = 1/(Ny_divs-1);
@@ -204,16 +204,15 @@ end
 %% plot the relevant lattice points to confirm correctness
 if plot_geom
     figure(1)
-%     scatter3(gcoord(inl,1),gcoord(inl,2),gcoord(inl,3),'r.');
-%     hold on
-%     scatter3(gcoord(onl,1),gcoord(onl,2),gcoord(onl,3),'b.');
-     scatter3(gcoord(snl,1),gcoord(snl,2),gcoord(snl,3),'g.'); hold on;
-%     hold off
+     scatter3(gcoord(inl,1),gcoord(inl,2),gcoord(inl,3),'r.');
+     hold on
+     scatter3(gcoord(onl,1),gcoord(onl,2),gcoord(onl,3),'b.');
+  %   scatter3(gcoord(snl,1),gcoord(snl,2),gcoord(snl,3),'g.'); hold on;
+  %   hold off
     scatter3(gcoord(obst_list,1),gcoord(obst_list,2),gcoord(obst_list,3),'b.');
-     axis([0 Lx_p 0 Ly_p 0 Lz_p]);
+    axis([0 Lx_p 0 Ly_p 0 Lz_p]);
     axis equal
     view([-99 52]);
-   
 end
 
 %% save the data to a *.mat file
