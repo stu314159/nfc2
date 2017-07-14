936c1314afb62bb62eba21984a6ead1aa5d92e35
Stu Blair <stu314159@gmail.com>
2017-07-14 09:15:55 -0500

    adjusted the dimensions of channel and brick to correspond to paper

--- wmb_geom.py
+++ wmb_geom.py
@@ -9,17 +9,17 @@ Created on Fri Jul 14 08:54:21 2017
 import FluidChannel as fc
 
 #overall channel dimensions
-aLx_p = 1.0
-aLy_p = 1.0
-aLz_p = 10.0
+aLx_p = 6.4
+aLy_p = 3.0
+aLz_p = 14.0
 aNdivs = 10
 
 # wall mounted brick parameters
-x_c = aLx_p/2.;
-z_c = (0.4)*aLz_p;
-W = aLx_p/3.;
-H = aLy_p/3.;
-L = aLz_p/10.;
+x_c = 3.5;
+z_c = 3.2;
+W = 1.;
+H = W;
+L = W;
 
 myObst = fc.WallMountedBrick(x_c,z_c,L,W,H);
 
